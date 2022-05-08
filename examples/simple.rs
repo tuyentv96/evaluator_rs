7d29ebda83041a51e32583e53f041abce74b3150
Tuyen Tran <vat1906@gmail.com>
2022-05-08 17:26:12 +0700

    Fix tests

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,11 +2,11 @@ extern crate evaluator_rs;
 
 use std::collections::HashMap;
 
-use evaluator_rs as evaluator;
+use evaluator_rs::{evaluate, parse_expr, Value};
 
 fn main() {
-    let expr = evaluator::parse_expr("1 + 2 + 3").unwrap();
+    let expr = parse_expr("1 + 2 + 3").unwrap();
     let parameters = HashMap::new();
-    let rs = evaluator::evaluate(&expr, &parameters).unwrap();
-    assert_eq!(rs, ast::Value::Number(6_f64));
+    let rs = evaluate(&expr, &parameters).unwrap();
+    assert_eq!(rs, Value::from(6));
 }
