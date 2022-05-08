337844758eac4f57468949e4e8f5114c4bf7bb43
Tuyen Tran <vat1906@gmail.com>
2022-05-08 17:55:20 +0700

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # evaluator_rs
 [![evaluator_rs](https://github.com/tuyentv96/evaluator_rs/actions/workflows/.test.yml/badge.svg)](https://github.com/tuyentv96/evaluator_rs/actions/workflows/.test.yml) [![codecov](https://codecov.io/gh/tuyentv96/evaluator_rs/branch/master/graph/badge.svg?token=VIyh6tcPDv)](https://codecov.io/gh/tuyentv96/evaluator_rs)
+
+## Operators
+
+| Operator | Description |
+|----------|-------------|
+| && | And |
+| || | Or |
+| == | Equal |
+| > | Greater than |
+| >= | Greater than or equal |
+| < | Lower than |
+| <= | Lower than or qual |
+| + | Sum |
+| - | Sub |
+| * | Product |
+| / | Division |
+| in | Array contains |
+
--- ast/src/value.rs
+++ ast/src/value.rs
@@ -21,42 +21,112 @@ impl Debug for Value {
     }
 }
 
+/// Convert String to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let s: String = "hello".to_string();
+/// let x: Value = s.into();
+/// ```
 impl From<String> for Value {
     fn from(string: String) -> Self {
         Value::String(string)
     }
 }
 
+/// Convert &str to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let s: str = "hello";
+/// let x: Value = s.into();
+/// ```
 impl From<&str> for Value {
     fn from(string: &str) -> Self {
         Value::String(string.to_string())
     }
 }
 
+/// Convert 64-bit float to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let f: f64 = 1.2;
+/// let x: Value = f.into();
+/// ```
 impl From<f64> for Value {
     fn from(float: f64) -> Self {
         Value::Number(float)
     }
 }
 
+/// Convert 32-bit float to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let f: f64 = 1.2;
+/// let x: Value = f.into();
+/// ```
 impl From<f32> for Value {
     fn from(float: f32) -> Self {
         Value::Number(float as f64)
     }
 }
 
+/// Convert 64-bit integer to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let i: i64 = 123;
+/// let x: Value = i.into();
+/// ```
 impl From<i64> for Value {
     fn from(int: i64) -> Self {
         Value::Number(int as f64)
     }
 }
 
+/// Convert 32-bit integer to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let i: i32 = 123;
+/// let x: Value = i.into();
+/// ```
 impl From<i32> for Value {
     fn from(int: i32) -> Self {
         Value::Number(int as f64)
     }
 }
 
+/// Convert boolean to `Value`
+///
+/// # Examples
+///
+/// ```
+/// use evaluator_rs::Value;
+///
+/// let b: bool = true;
+/// let x: Value = b.into();
+/// ```
 impl From<bool> for Value {
     fn from(boolean: bool) -> Self {
         Value::Bool(boolean)
